016d1f57341e7568571268937d6997dcf5d7f886
youngsupchoi <sup1214@naver.com>
2024-12-04 09:51:42 +0900

    feat: 로그인 api연결

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,32 +1,32 @@
-import axios from "axios";
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
-
-export const createUser = async (user: any) => {
-  try {
-    const response = await axios.post(`user/create`, { user });
-    return response.data;
-  } catch (error) {
-    console.error("API 요청 에러:", error);
-    throw error;
-  }
-};
-
 // import axios from "axios";
 // axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 // export const createUser = async (user: any) => {
 //   try {
-//     const response = await axios.post(
-//       `https://monthly-madge-choco-planner-59fb550a.koyeb.app/api/user/create`,
-//       {
-//         email: user.email,
-//         // given_name: user.given_name,
-//         // family_name: user.family_name,
-//       },
-//     );
+//     const response = await axios.post(`user/create`, { user });
 //     return response.data;
 //   } catch (error) {
 //     console.error("API 요청 에러:", error);
 //     throw error;
 //   }
 // };
+
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+export const createUser = async (user: any) => {
+  try {
+    const response = await axios.post(
+      `https://monthly-madge-choco-planner-59fb550a.koyeb.app/api/user/create`,
+      {
+        email: user.email,
+        // given_name: user.given_name,
+        // family_name: user.family_name,
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.error("API 요청 에러:", error);
+    throw error;
+  }
+};
